2053bce6e9e8c900742dfb6df2aa0b9a4e224bd8
ClementDhennin <clementdhe@gmail.com>
2026-09-16 11:50:44 +0200

    TEST

---
+++ TP2Classe.java
@@ -0,0 +1,15 @@
+public class TP2Classe {
+    public class Vecteur {
+        private String Nom;
+        private Double x;
+        private Double y;
+
+        public Vecteur (String Nom, Double x,Double y){
+
+        }
+        
+        public void Affiche (){
+
+        }
+    }
+}
